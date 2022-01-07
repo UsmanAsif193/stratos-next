81132d0f75bb90b41e2b6bce62f3cf5f95d873c4
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-01-07 18:18:12 +0500

    section 2 heading added

--- src/components/questionList/index.js
+++ src/components/questionList/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const QuestionList = () => {
-    return (
-        <div>
-            <img src="../planet1.png" alt="" />
+  return (
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-2 p-20 h-screen">
+      <div>
+        <div className="flex items-center">
+          <div>
+            <img className="w-[90px] ml-20 mb-4" src="assets/planet1.png" />
+            <img className="w-[185px]" src="assets/planet2.png" />
+          </div>
+          <div>
+            <h2 className=" text-3xl ml-10 font-bold">
+              A unique networking experience that is{" "}
+              <span> out of this world. </span>
+            </h2>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div>
+        <h2>JOIN THE QUESTLIST</h2>
+      </div>
+    </div>
+  );
+};
 
-export default QuestionList
+export default QuestionList;
