b5d65859aaa4881d3153a7623f8363352b414157
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-07 17:47:49 +0500

    navbar font size reduced

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center text-xl font-normal justify-between pt-8 px-24">
+    <div className="flex items-center text-lg font-normal justify-between pt-8 px-24">
       <h1 className="tracking-[.7rem] uppercase font-extralight">Stratos</h1>
       <div>
         <ul className="flex">
