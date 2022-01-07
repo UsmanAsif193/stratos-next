0dd006faf58fc6b63cee32c9dc6d1698c2e5cb99
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-07 18:51:43 +0500

    feat: navbar responsiveness added

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -27,16 +27,20 @@ const Hero = () => {
     setDateToGet("01/29/2022");
   });
   return (
-    <div className="text-lg flex flex-col justify-evenly items-center h-5/6">
-      <h1 className="tracking-[1.6rem] uppercase font-extralight text-2xl">
+    <div className="text-lg flex flex-col justify-evenly items-center h-5/6 pt-12">
+      <h1 className="tracking-[1.6rem] uppercase font-extralight text-xl text-center sm:text-2xl">
         Stratos Presents
       </h1>
-      <img src="/assets/Cloud-1.png" alt="Social_Club" className="h-[150px] " />
-      <h2 className="font-light tracking-[.6rem] text-2xl">
+      <img
+        src="/assets/Cloud-1.png"
+        alt="Social_Club"
+        className="lg:h-[130px] 2xl:h-[150px] "
+      />
+      <h2 className="text-center font-light tracking-[.6rem] text-2xl">
         FIRST LINK UP OF THE YEAR:{" "}
         <span className="font-normal">ATLANTA, GA</span>
       </h2>
-      <div className="flex items-center justify-center text-3xl">
+      <div className="items-center justify-center text-3xl hidden md:flex">
         <div className="flex flex-col items-center justify-center mx-8">
           <span className="text-7xl font-bold">{timeLeft.days}</span>Days
         </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,24 +1,75 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+
+const NavbarItem = ({ title, classProps, link }) => {
+  return (
+    <Link href={link}>
+      <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>
+    </Link>
+  );
+};
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className="flex items-center text-lg font-normal justify-between pt-8 px-24">
-      <h1 className="tracking-[.7rem] uppercase font-extralight">Stratos</h1>
-      <div>
-        <ul className="flex">
-          <li className="mx-6 cursor-pointer">
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li className="mx-6 cursor-pointer">
-            <Link href={"/"}>About Stratos</Link>
-          </li>
-          <li className="mx-6 cursor-pointer">
-            <Link href={"/"}>The Social Club</Link>
-          </li>
-        </ul>
+    <div className="pt-8 md:px-24 px-8">
+      <div className="items-center text-lg font-normal justify-between hidden md:flex">
+        <h1 className="tracking-[.7rem] uppercase font-extralight">Stratos</h1>
+        <div>
+          <ul className="flex">
+            <NavbarItem title={"Home"} link="/" />
+            <NavbarItem title={"About Stratos"} link="/" />
+            <NavbarItem title={"The Social Club"} link="/" />
+          </ul>
+        </div>
+        <div className="cursor-pointer">
+          <NavbarItem title={"Contact Us"} classProps={"list-none"} link="/" />
+        </div>
       </div>
-      <div>Contact Us</div>
+      <div className=" md:hidden">
+        <div className="flex justify-between">
+          <h1 className="tracking-[.7rem] uppercase font-extralight">
+            Stratos
+          </h1>
+          <i
+            className="fa fa-bars text-white cursor-pointer"
+            onClick={() => setToggleMenu(true)}
+          ></i>
+        </div>
+        <div>
+          {toggleMenu && (
+            <ul
+              className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none 
+          flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
+          "
+            >
+              <li className="text-xl w-full my-2">
+                <i
+                  className="fas fa-times"
+                  onClick={() => setToggleMenu(false)}
+                />
+              </li>
+
+              <NavbarItem title={"Home"} link="/" classProps="my-2 text-lg" />
+              <NavbarItem
+                title={"About Stratos"}
+                link="/"
+                classProps="my-2 text-lg"
+              />
+              <NavbarItem
+                title={"The Social Club"}
+                link="/"
+                classProps="my-2 text-lg"
+              />
+              <NavbarItem
+                title={"Contact Us"}
+                classProps={"list-none my-2 text-lg"}
+                link="/"
+              />
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
