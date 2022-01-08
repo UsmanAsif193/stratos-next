f37ffc30015ed29fafea7987580167fa85590a0c
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-08 17:25:29 +0500

    fix: contact us page mobile design update

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -5,7 +5,7 @@ import Navbar from "../components/navbar";
 const contactUs = () => {
   return (
     <div className="min-h-screen">
-      <div className="min-h-screen h-full bg-cloud-image-index-2 bg-cover text-white bg-blend-soft-light lg:bg-blend-normal bg-blend-normal">
+      <div className="h-full bg-cloud-image-index-2 bg-cover text-white bg-blend-soft-light lg:bg-blend-normal bg-blend-normal">
         <Navbar blackColor />
         <JoinStratos />
       </div>
