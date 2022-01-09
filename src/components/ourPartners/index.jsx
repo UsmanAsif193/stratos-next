f7f7ba3d07f08804506b323c7ba98871273c6927
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-09 15:34:37 +0500

    fix: our-partners heading centered

--- src/components/ourPartners/index.jsx
+++ src/components/ourPartners/index.jsx
@@ -14,7 +14,7 @@ const OurPartners = () => {
     <Zoom left>
       <section className="p-20 px-12 min-h-screen md:h-screen">
         <div className="flex justify-center items-center">
-          <h1 className="tracking-[.7rem] uppercase font-light text-2xl md:text-3xl">
+          <h1 className="tracking-[.7rem] uppercase font-light text-2xl md:text-3xl text-center">
             OUR PARTNERS
           </h1>
         </div>
